3778a00a88b134b0b2403e9bf71060ab490dc38a
Code-Gokul <gokulrajmaster@gmail.com>
2024-08-27 16:49:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<body>
+  <h1>CSS Exercise-1</h1>
+  <center><h3>Desktop View</h3></center>
+<img src="/images/laptop.png">
+  <center><h3>Tablet View</h3></center>
+  <img src="/images/tablet.png" width="50%">
+    <center><h3>Mobile View</h3></center>
+  <img src="/images/mobile.png" width="25%">
+</body>     
---
+++ js/script.js
@@ -0,0 +1,126 @@
+$(document).ready(function () {
+  //Adding a new div called references before the footer and add a few references in it.
+  $("<div/>", {
+    id: "references",
+    html: `
+                 <main class="center-content">
+                        <div class="hidden">
+                            <h4>References</h4>
+                        </div>
+                        <ul class="list">
+                            <li>John Doe - Front-end Developer at Tech Corp</li>
+                            <li>Jane Smith - Senior Manager at XYZ Ltd.</li>
+                            <li>Michael Brown - Lead Developer at WebWorks</li>
+                        </ul>
+                    </main>
+            `,
+  }).appendTo("#mypage_centerContent");
+
+  $("<aside>", {
+    class: "leftnav",
+    html: `<h4>References</h4>`,
+  }).appendTo("#mypage_leftnav");
+
+  //Changing the background color to #FFFF88 | border color to #FF1A00 | text to #CC0000.
+  $("#mypage_header nav").css({
+    "background-color": "#FFFF88",
+    "border-color": "#FF1A00",
+    color: "#CC0000",
+  });
+
+  $("#mypage_leftnav aside").css({
+    "background-color": "#FFFF88",
+    "border-color": "#FF1A00",
+  });
+
+  $(".leftnav h4 ").css({
+    color: "#CC0000",
+  });
+
+  $(".hidden").css({
+    "background-color": "#FFFF88",
+  });
+
+  $(".hidden h4").css({
+    color: "#CC0000",
+  });
+
+  $("#mypage_centerContent main").css({
+    "background-color": "#FFFF88",
+    color: "#CC0000",
+  });
+
+  $(".year").css({
+    color: "#CC0000",
+  });
+
+  $(".center-content").css({
+    "border-color": "#FF1A00",
+  });
+
+  $(".list li").css({
+    "border-color": "#FF1A00",
+  });
+
+  $("table").css({
+    "border-color": "#FF1A00",
+    color: "#CC0000",
+  });
+
+
+
+  // Hiding the element with ID mypage_footer
+  // $("#mypage_footer").hide();
+
+  //--------------------------jQuery Exercise 2-----------------------------------------
+
+  //changing the mypage-header to have height of 10px
+  $("#mypage_header nav").css({
+    height: "10px",
+  });
+
+  //adding an "mouseenter" event handler to increase the size to the normal when mouse enters header
+  $("nav").mouseenter(function () {
+    $("#mypage_header nav").css({
+      height: "100px",
+    });
+  });
+
+  //adding an "mouseleave" event handler to reduce back the size to 10px when mouse leaves header
+
+  $("nav").mouseleave(function () {
+    $("#mypage_header nav").css({
+      height: "10px",
+    });
+  });
+
+  // Initially hide the footer
+
+  $("footer").css({
+    display: "none", // Ensure it is hidden initially
+    height: "0", // Set height to 0
+  });
+
+ 
+  // On mouseenter, slide down the footer and show the alert
+
+  $("#mypage_footer").on("mouseenter", function () {
+    $("footer").css({
+      height: "auto",
+    }),
+
+    $("footer")
+      .stop()
+      .slideDown(10000, function () {
+        alert("Animation is Completed!");
+      });
+  });
+
+  // On mouseleave, slide back the footer and hide it
+  $("#mypage_footer").mouseleave(function () {
+    $("footer")
+      .stop(true, true)
+      .slideUp(500);
+  });
+
+});
